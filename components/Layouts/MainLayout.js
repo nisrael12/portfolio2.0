e7e5aad88bac9638debfe9c60042bb0978b686a5
nisrael12 <dev@naviisrael.com>
2022-04-04 21:29:23 -0500

    Mobile Nav added to UI. Responsive worked needed for large and small phone.

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -1,11 +1,13 @@
 import Footer from "../UI/Footer";
 import Header from "../UI/Header";
+import MobileNav from "../UI/MobileNav";
 
 const MainLayout = (props) => {
   return (
     <>
       <section className="wrapper" id="wrapper">
         <Header />
+        <MobileNav />
         {props.children}
         <Footer />
       </section>
---
+++ components/UI/MobileNav.js
@@ -0,0 +1,41 @@
+const MobileNav = (props) => {
+  return (
+    <div id="mobile-nav">
+      <div className="mobile-nav__nav">
+        <ul>
+          <li id="mobile-nav__nav-about">
+          <a>
+            <div className="mobile-nav__icon-border">
+              <i className="fa-solid fa-face-grin-wide"></i>
+            </div>
+           About Me</a>
+          </li>
+          <li id="mobile-nav__nav-portfolio">
+          <a>
+            <div className="mobile-nav__icon-border">
+              <i className="fa-solid fa-book-open"></i>
+            </div>
+            Portfolio</a>
+          </li>
+          <li id="mobile-nav__nav-skills">
+          <a>
+            <div className="mobile-nav__icon-border">
+              <i className="fa-solid fa-screwdriver-wrench"></i>
+            </div>
+            Skills</a>
+          </li>
+          <li id="mobile-nav__nav-jobs">
+          <a>
+            <div className="mobile-nav__icon-border">
+              <i className="fa-solid fa-user-tie"></i>
+            </div>
+
+            Jobs</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
